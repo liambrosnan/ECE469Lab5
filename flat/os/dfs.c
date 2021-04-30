8aad03049d9c6a6202a4f962394caa921071e091
Liam Thomas Brosnan <lbrosnan@ecegrid-thin1.ecn.purdue.edu>
2021-04-30 00:23:11 -0400

    bug fixes

--- flat/os/dfs.c
+++ flat/os/dfs.c
@@ -6,39 +6,39 @@
 #include "dfs.h"
 #include "synch.h"
 
-#include "dfs_shared.h"
+// #include "dfs_shared.h"
 
-#define DFS_INODE_NMAX_NUM 128
-#define DFS_MAX_NUM_WORDS (1024 * 8) / 4
+// #define DFS_INODE_NMAX_NUM 128
+// #define DFS_MAX_NUM_WORDS (1024 * 8) / 4
 
-#define DFS_INODE_MAX_FILENAME_LENGTH 72
-#define DFS_INODE_BLOCKTABLE_SIZE 10
+// #define DFS_INODE_MAX_FILENAME_LENGTH 72
+// #define DFS_INODE_BLOCKTABLE_SIZE 10
 
-typedef struct dfs_superblock {
-    // STUDENT: put superblock internals here
-    int valid;
-    int blocksize;
-    int blocks;
-    int num_inodes;
-    int inodeBlockStart;
-    int fbvBlockStart;
-    int dataBlockStart;
-} dfs_superblock;
+// typedef struct dfs_superblock {
+//     // STUDENT: put superblock internals here
+//     int valid;
+//     int blocksize;
+//     int blocks;
+//     int num_inodes;
+//     int inodeBlockStart;
+//     int fbvBlockStart;
+//     int dataBlockStart;
+// } dfs_superblock;
 
-typedef struct dfs_inode {
-    // STUDENT: put inode structure internals here
-    // IMPORTANT: sizeof(dfs_inode) MUST return 128 in order to fit in enough
-    // inodes in the filesystem (and to make your life easier).  To do this,
-    // adjust the maximumm length of the filename until the size of the overall inode
-    // is 128 bytes.
+// typedef struct dfs_inode {
+//     // STUDENT: put inode structure internals here
+//     // IMPORTANT: sizeof(dfs_inode) MUST return 128 in order to fit in enough
+//     // inodes in the filesystem (and to make your life easier).  To do this,
+//     // adjust the maximumm length of the filename until the size of the overall inode
+//     // is 128 bytes.
     
-    int in_use;
-    int file_size;
-    char filename[DFS_INODE_MAX_FILENAME_LENGTH];
-    int blockTable[DFS_INODE_BLOCKTABLE_SIZE];
-    int BTindex;
-    int BTindex2;
-} dfs_inode;
+//     int in_use;
+//     int file_size;
+//     char filename[DFS_INODE_MAX_FILENAME_LENGTH];
+//     int blockTable[DFS_INODE_BLOCKTABLE_SIZE];
+//     int BTindex;
+//     int BTindex2;
+// } dfs_inode;
 
 
 
